6322600c5c32ad19c727dde87c450ca9813d7394
jsphill8_ncstate <jsphill8_ncstate>
2026-05-06 01:26:55 -0400

    basic scripting setup. Get each of the timestamps, camera infos and depths to separate into one object. include more than one library for use in script.

--- replication/scripts/original-data-to-tum.py
+++ replication/scripts/original-data-to-tum.py
@@ -1,3 +1,23 @@
-import scipy.spatial.transform.Rotation
+from scipy.spatial.transform import Rotation
+import pathlib
+import json
+from Pillow import Image
 
-def original_to_tum(indir, outdir):
+def original_to_tum(indir: pathlib.Path, outdir: pathlib.Path):
+    keydir = indir / "keyframes"
+    cam_dir   = keydir / "cameras"
+    img_dir   = keydir / "images"
+    depth_dir = keydir / "depth"
+
+    out_rgb   = outdir / "rgb"
+    out_depth = outdir / "depth"
+    out_rgb.mkdir(parents=True, exist_ok=True)
+    out_depth.mkdir(parents=True, exist_ok=True)
+    
+    for f in cam_dir.iterdir():
+        fname = f.stem
+
+        current_camera = json.loads(f.read_text())
+        current_image = Image.open(img_dir / (fname + ".jpg")).convert("RGB")
+        current_depth = Image.open(depth_dir / (fname + ".jpg"))
+
